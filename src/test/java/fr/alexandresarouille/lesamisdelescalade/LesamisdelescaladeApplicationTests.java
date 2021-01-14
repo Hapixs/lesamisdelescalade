47819e846d8bab119d52cbda46fc298c2092e6c2
Hapixs <alexs2341@gmail.com>
2021-01-14 21:54:53 +0100

    Configuration de springboot

---
+++ src/test/java/fr/alexandresarouille/lesamisdelescalade/LesamisdelescaladeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alexandresarouille.lesamisdelescalade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesamisdelescaladeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
